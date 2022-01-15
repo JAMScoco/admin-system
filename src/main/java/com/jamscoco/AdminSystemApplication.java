4c0152a9f82b74b9bb9ab92bb2c5802d8d62047d
JAMScoco <xukungou@gmail.com>
2022-01-15 13:37:34 +0800

    init

---
+++ src/main/java/com/jamscoco/AdminSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jamscoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jamscoco/AdminSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamscoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
